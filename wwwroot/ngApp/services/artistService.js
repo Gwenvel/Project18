51e53df4394a9e6daaf0ee67f7057c12fff761b5
Joy Kimaru <Joy Kimaru>
2017-09-14 15:34:59 -0700

    got a view

--- wwwroot/ngApp/services/artistService.js
+++ wwwroot/ngApp/services/artistService.js
@@ -1,6 +1,6 @@
 class ArtistService {
     constructor($resource) {
-        this.artistResource = $resource("http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=87bdb2c24f5d7ea2e34ac5d1bdc419f1&format=json&limit=5");
+        this.artistResource = $resource("/api/artists:id");
     }
 
     getArtists() {
