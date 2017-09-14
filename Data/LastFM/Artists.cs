6525db45e4d090d63ee63088c0c700b6c794e12b
dbronnenkant <drew@bronnenmail.com>
2017-09-14 14:58:21 -0700

    built backend to return LastFM

---
+++ Data/LastFM/Artists.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace Project18.Data.LastFM
+{
+    [DataContract(Name = "artists")]
+    public class Artists
+    {
+        public Artists()
+        {
+            ArtistList = new List<Artist>();
+        }
+
+        [DataMember(Name = "artist")]
+        public List<Artist> ArtistList { get; set; }
+    }
+}
---
+++ Data/LastFM/Image.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace Project18.Data.LastFM
+{
+[DataContract(Name = "image")]
+        public class Image
+        {
+            [DataMember(Name = "#text")]
+            public string Text { get; set; }
+ 
+            [DataMember(Name = "size")]
+            public string Size { get; set; }
+        }
+}
